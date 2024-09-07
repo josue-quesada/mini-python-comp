6e307c10ed3065b37412b24233168b1f5a916da5
Josué Quesada <josuequesadaxd@gmail.com>
2024-09-06 21:23:47 -0600

    updated ui

---
+++ mini-python/UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+}
